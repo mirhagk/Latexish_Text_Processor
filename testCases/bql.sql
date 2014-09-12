ad1758b2345b8492e7b0fefd211fbc5e52764fbb
mirhagk <mirhagk@gmail.com>
2014-09-12 16:32:34 -0400

    Added new test cases

--- testCases/bql.sql
+++ testCases/bql.sql
@@ -1,14 +1,13 @@
 
-
-
-
+--Update all employee
 update dbo.employee set employeeid = 100
 
-
+--for loop, print index
 
 DECLARE @index_middle
 SET @index_middle = 1
 WHILE @index_middle < 3
 BEGIN
-print @index_middle
+	print @index_middle
+	SET @index_middle = @index_middle + 1
 END
